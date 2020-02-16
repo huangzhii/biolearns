3749e1f807a3bfaefcf439fe1dd2fedcb0b9c977
huangzhii <huangzz@umich.edu>
2020-02-16 15:21:48 -0500

    update from DELL 2060 laptop

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ from biolearns.coexpression.lmQCM import lmQCM
 from biolearns.preprocessing.filter import expression_filter
 from biolearns.survival import logranktest
 
-brca = TCGACancer('LAML')
+brca = TCGACancer('BRCA')
 mRNAseq = brca.mRNAseq
 clinical = brca.clinical
 
@@ -21,22 +21,25 @@ clinical = brca.clinical
 # =============================================================================
 mRNAseq = expression_filter(mRNAseq, meanq = 0.5, varq = 0.5)
 lobj = lmQCM(mRNAseq)
-lobj.fit()
+clusters, genes, eigengene_mat = lobj.fit()
+
 
 
 # =============================================================================
 # Survival Analysis
 # =============================================================================
-genes = mRNAseq.index.values.astype('str')
-ratio = mRNAseq.loc['AAK1',].values
+
+r = mRNAseq.loc['ABLIM3',].values
 
 bcd_m = [b[:12] for b in mRNAseq.columns]
 bcd_p = [b[:12] for b in clinical.index]
 bcd = np.intersect1d(bcd_m, bcd_p)
 
-ratio = ratio[np.nonzero(np.in1d(bcd_m, bcd))[0]]
-t = brca.overall_survival_time[np.nonzero(np.in1d(bcd_p, bcd))[0]]
-e = brca.overall_survival_event[np.nonzero(np.in1d(bcd_p, bcd))[0]]
+r = r[np.nonzero(np.in1d(bcd, bcd_m))[0]]
+t = brca.overall_survival_time[np.nonzero(np.in1d(bcd, bcd_p))[0]]
+e = brca.overall_survival_event[np.nonzero(np.in1d(bcd, bcd_p))[0]]
 
-logrank_results, fig = logranktest(ratio[~np.isnan(t)], t[~np.isnan(t)], e[~np.isnan(t)])
+logrank_results, fig = logranktest(r[~np.isnan(t)], t[~np.isnan(t)], e[~np.isnan(t)])
 
+test_statistic, p_value = logrank_results.test_statistic, logrank_results.p_value
+    
