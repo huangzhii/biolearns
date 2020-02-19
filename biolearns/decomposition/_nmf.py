09d5a4d164fe560023c97144d08b89d3090a347d
huangzhii <huangzz@umich.edu>
2020-02-19 14:45:09 -0500

    update from DELL 2060 laptop

--- biolearns/decomposition/_nmf.py
+++ biolearns/decomposition/_nmf.py
@@ -18,12 +18,13 @@
 # such damage.
 #
 import numpy as np
-import scipy as sp
+import scipy.sparse as sp
 import pandas as pd
-from scipy import sparse
 from sklearn.utils import check_random_state, check_array
 from sklearn.decomposition._nmf import _initialize_nmf
 from sklearn.decomposition._cdnmf_fast import _update_cdnmf_fast
+from sklearn.utils.extmath import safe_sparse_dot
+
 from lifelines.utils import concordance_index
 from ..survival import StepCoxPHFitter
 import time
@@ -100,48 +101,6 @@ def calcuate_Frobenius_norm(X, W, H, square_root=False):
         return np.sqrt(res * 2)
     else:
         return res
-
-def safe_sparse_dot(a, b, dense_output=False):
-    # this is from sklearn.utils.extmath
-    """Dot product that handle the sparse matrix case correctly
-    
-    Parameters
-    ----------
-    a : array or sparse matrix
-    b : array or sparse matrix
-    dense_output : boolean, (default=False)
-        When False, ``a`` and ``b`` both being sparse will yield sparse output.
-        When True, output will always be a dense array.
-
-    Returns
-    -------
-    dot_product : array or sparse matrix
-        sparse if ``a`` and ``b`` are sparse and ``dense_output=False``.
-    """
-    if a.ndim > 2 or b.ndim > 2:
-        if sparse.issparse(a):
-            # sparse is always 2D. Implies b is 3D+
-            # [i, j] @ [k, ..., l, m, n] -> [i, k, ..., l, n]
-            b_ = np.rollaxis(b, -2)
-            b_2d = b_.reshape((b.shape[-2], -1))
-            ret = a @ b_2d
-            ret = ret.reshape(a.shape[0], *b_.shape[1:])
-        elif sparse.issparse(b):
-            # sparse is always 2D. Implies a is 3D+
-            # [k, ..., l, m] @ [i, j] -> [k, ..., l, j]
-            a_2d = a.reshape(-1, a.shape[-1])
-            ret = a_2d @ b
-            ret = ret.reshape(*a.shape[:-1], b.shape[1])
-        else:
-            ret = np.dot(a, b)
-    else:
-        ret = a @ b
-
-    if (sparse.issparse(a) and sparse.issparse(b)
-            and dense_output and hasattr(ret, "toarray")):
-        return ret.toarray()
-    return ret
-
 
 def _multiplicative_update_w(X, W, H, HHt=None, XHt=None, update_H=True):
     """update W in Multiplicative Update NMF"""
--- example.py
+++ example.py
@@ -8,18 +8,18 @@ Created on Wed Feb 12 15:11:25 2020
 
 import numpy as np
 from biolearns.dataset import TCGA
-from biolearns.coexpression import lmQCM
-from biolearns.preprocessing.filter import expression_filter
-from biolearns.survival import logranktest
+from biolearns.preprocessing import expression_filter
 
 brca = TCGA('BRCA')
 mRNAseq = brca.mRNAseq
 clinical = brca.clinical
+mRNAseq = expression_filter(mRNAseq, meanq = 0.5, varq = 0.5)
 
 # =============================================================================
 # Gene Co-expression Analysis
 # =============================================================================
-mRNAseq = expression_filter(mRNAseq, meanq = 0.5, varq = 0.5)
+from biolearns.coexpression import lmQCM
+
 lobj = lmQCM(mRNAseq)
 clusters, genes, eigengene_mat = lobj.fit()
 
@@ -28,6 +28,7 @@ clusters, genes, eigengene_mat = lobj.fit()
 # =============================================================================
 # Survival Analysis
 # =============================================================================
+from biolearns.survival import logranktest
 
 r = mRNAseq.loc['ABLIM3',].values
 
@@ -43,3 +44,20 @@ logrank_results, fig = logranktest(r[~np.isnan(t)], t[~np.isnan(t)], e[~np.isnan
 
 test_statistic, p_value = logrank_results.test_statistic, logrank_results.p_value
     
+
+
+# =============================================================================
+# CoxNMF
+# =============================================================================
+from biolearns.decomposition import CoxNMF
+
+bcd_m = [b[:12] for b in mRNAseq.columns]
+bcd_p = [b[:12] for b in clinical.index]
+bcd = np.intersect1d(bcd_m, bcd_p)
+
+X = mRNAseq.values[:,np.nonzero(np.in1d(bcd, bcd_m))[0]]
+t = brca.overall_survival_time[np.nonzero(np.in1d(bcd, bcd_p))[0]]
+e = brca.overall_survival_event[np.nonzero(np.in1d(bcd, bcd_p))[0]]
+n_components = 16
+
+CoxNMF(X, n_components=16, t=t, e=e)
