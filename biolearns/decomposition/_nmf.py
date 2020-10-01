971ff071fa8bd31ae452fa031972f31c324ffc36
Zhi Huang <huangzz@umich.edu>
2020-09-30 23:44:07 -0400

    Update from RTX TITAN

--- biolearns/decomposition/_nmf.py
+++ biolearns/decomposition/_nmf.py
@@ -27,6 +27,8 @@ from sklearn.decomposition.cdnmf_fast import _update_cdnmf_fast
 from sklearn.utils.extmath import safe_sparse_dot
 import copy
 
+from sklearn.utils.extmath import randomized_svd, safe_sparse_dot, squared_norm
+from sklearn.utils.validation import check_non_negative
 from lifelines.utils import concordance_index
 # from ..survival import StepCoxPHFitter
 from ..survival import newton_rhapson_for_efron_model
@@ -34,8 +36,146 @@ import time
 import warnings
 import logging
 EPSILON = np.finfo(np.float32).eps
+def norm(x):
+    """Dot product-based Euclidean norm implementation
+    See: http://fseoane.net/blog/2011/computing-the-vector-norm/
+    Parameters
+    ----------
+    x : array-like
+        Vector for which to compute the norm
+    """
+    return sqrt(squared_norm(x))
 
     
+def _initialize_nmf(X, n_components, init=None, eps=1e-6,
+                    random_state=None):
+    """Algorithms for NMF initialization.
+    Computes an initial guess for the non-negative
+    rank k matrix approximation for X: X = WH
+    Parameters
+    ----------
+    X : array-like, shape (n_samples, n_features)
+        The data matrix to be decomposed.
+    n_components : integer
+        The number of components desired in the approximation.
+    init :  None | 'random' | 'nndsvd' | 'nndsvda' | 'nndsvdar'
+        Method used to initialize the procedure.
+        Default: None.
+        Valid options:
+        - None: 'nndsvd' if n_components <= min(n_samples, n_features),
+            otherwise 'random'.
+        - 'random': non-negative random matrices, scaled with:
+            sqrt(X.mean() / n_components)
+        - 'nndsvd': Nonnegative Double Singular Value Decomposition (NNDSVD)
+            initialization (better for sparseness)
+        - 'nndsvda': NNDSVD with zeros filled with the average of X
+            (better when sparsity is not desired)
+        - 'nndsvdar': NNDSVD with zeros filled with small random values
+            (generally faster, less accurate alternative to NNDSVDa
+            for when sparsity is not desired)
+        - 'custom': use custom matrices W and H
+    eps : float
+        Truncate all values less then this in output to zero.
+    random_state : int, RandomState instance, default=None
+        Used when ``init`` == 'nndsvdar' or 'random'. Pass an int for
+        reproducible results across multiple function calls.
+        See :term:`Glossary <random_state>`.
+    Returns
+    -------
+    W : array-like, shape (n_samples, n_components)
+        Initial guesses for solving X ~= WH
+    H : array-like, shape (n_components, n_features)
+        Initial guesses for solving X ~= WH
+    References
+    ----------
+    C. Boutsidis, E. Gallopoulos: SVD based initialization: A head start for
+    nonnegative matrix factorization - Pattern Recognition, 2008
+    http://tinyurl.com/nndsvd
+    """
+    check_non_negative(X, "NMF initialization")
+    n_samples, n_features = X.shape
+
+    if (init is not None and init != 'random'
+            and n_components > min(n_samples, n_features)):
+        raise ValueError("init = '{}' can only be used when "
+                         "n_components <= min(n_samples, n_features)"
+                         .format(init))
+
+    if init is None:
+        if n_components <= min(n_samples, n_features):
+            init = 'nndsvd'
+        else:
+            init = 'random'
+
+    # Random initialization
+    if init == 'random':
+        avg = np.sqrt(X.mean() / n_components)
+        rng = check_random_state(random_state)
+        H = avg * rng.randn(n_components, n_features).astype(X.dtype,
+                                                             copy=False)
+        W = avg * rng.randn(n_samples, n_components).astype(X.dtype,
+                                                            copy=False)
+        np.abs(H, out=H)
+        np.abs(W, out=W)
+        return W, H
+
+    # NNDSVD initialization
+    U, S, V = randomized_svd(X, n_components, random_state=random_state)
+    W = np.zeros_like(U)
+    H = np.zeros_like(V)
+
+    # The leading singular triplet is non-negative
+    # so it can be used as is for initialization.
+    W[:, 0] = np.sqrt(S[0]) * np.abs(U[:, 0])
+    H[0, :] = np.sqrt(S[0]) * np.abs(V[0, :])
+
+    for j in range(1, n_components):
+        x, y = U[:, j], V[j, :]
+
+        # extract positive and negative parts of column vectors
+        x_p, y_p = np.maximum(x, 0), np.maximum(y, 0)
+        x_n, y_n = np.abs(np.minimum(x, 0)), np.abs(np.minimum(y, 0))
+
+        # and their norms
+        x_p_nrm, y_p_nrm = norm(x_p), norm(y_p)
+        x_n_nrm, y_n_nrm = norm(x_n), norm(y_n)
+
+        m_p, m_n = x_p_nrm * y_p_nrm, x_n_nrm * y_n_nrm
+
+        # choose update
+        if m_p > m_n:
+            u = x_p / x_p_nrm
+            v = y_p / y_p_nrm
+            sigma = m_p
+        else:
+            u = x_n / x_n_nrm
+            v = y_n / y_n_nrm
+            sigma = m_n
+
+        lbd = np.sqrt(S[j] * sigma)
+        W[:, j] = lbd * u
+        H[j, :] = lbd * v
+
+    W[W < eps] = 0
+    H[H < eps] = 0
+
+    if init == "nndsvd":
+        pass
+    elif init == "nndsvda":
+        avg = X.mean()
+        W[W == 0] = avg
+        H[H == 0] = avg
+    elif init == "nndsvdar":
+        rng = check_random_state(random_state)
+        avg = X.mean()
+        W[W == 0] = abs(avg * rng.randn(len(W[W == 0])) / 100)
+        H[H == 0] = abs(avg * rng.randn(len(H[H == 0])) / 100)
+    else:
+        raise ValueError(
+            'Invalid init parameter: got %r instead of one of %r' %
+            (init, (None, 'random', 'nndsvd', 'nndsvda', 'nndsvdar')))
+
+    return W, H
 def trace_dot(X, Y):
     """Trace of np.dot(X, Y.T).
 
